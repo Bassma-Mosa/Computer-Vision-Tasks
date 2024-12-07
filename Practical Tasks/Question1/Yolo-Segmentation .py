7a54f4fde8aee76c6d346089f339c9703e9d40a2
Bassma Mosa <bassmamosa2@gmail.com>
2024-12-07 16:01:21 +0200

    Update Yolo-Segmentation .py

--- Practical Tasks/Question1/Yolo-Segmentation .py
+++ Practical Tasks/Question1/Yolo-Segmentation .py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 model = YOLO("yolov8n-seg.pt")
 
 results=model(source="image.jpg",show=True,conf=0.4,save=True)
-# results=model(source="C:/Users/asd432/Desktop/videoo.mp4",show=True,conf=0.4,save=True)
+# results=model(source="C:/Users/asd432/Desktop/video.mp4",show=True,conf=0.4,save=True)
 
 result_img = results[0].plot()
 plt.imshow(result_img)
